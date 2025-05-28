c3676be58f8ca0c47368ec81df2ddd4963b1dec6
malindesire <malindesirejohansson@gmail.com>
2025-05-28 12:12:46 +0200

    Add PriceLevel enum

---
+++ FlowControl/PriceLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowControl
+{
+    internal enum PriceLevel
+    {
+        Regular = 120,
+        Youth = 80,
+        Senior = 90,
+    }
+}
